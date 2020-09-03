c5f4c9e92dc0203c40cba5a2b0bd73992114700f
Jonathan Thomaz <jonathanthomaz96@gmail.com>
2020-09-03 15:54:59 -0300

    add detailsTrilha

--- server/src/models/TrilhaTeste.js
+++ server/src/models/TrilhaTeste.js
@@ -2,9 +2,16 @@ const mongoose = require('mongoose');
 
 const TrilhaTesteSchema = new mongoose.Schema({
     title: String,
-    coodernadasCamera: {latitute:Number,longitude:Number},
+    coodernadasCamera: {
+        latitute: Number,
+        longitude: Number
+    },
     geometryPropietes: Object,
-
+    detailsTrilha: {
+        observations: String,
+        difficult: String,
+        distance: String,
+    }
 });
 
 mongoose.model('TrilhaTeste', TrilhaTesteSchema);
